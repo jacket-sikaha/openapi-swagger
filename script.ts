d1f74177ce7f59a474bb421ba7fbc461b74d3807
sikara <xuhaoxian@gdlinkjob.com>
2024-02-18 10:30:01 +0800

    try Prisma

---
+++ script.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Aliasdce",
+  //       email: "aliasdasdce@prisma.io",
+  //     },
+  //   });
+  //   console.log(user);
+
+  //   const users = await prisma.user.findMany();
+  //   console.log(users);
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Bob34",
+      email: "bob34@prisma.io",
+      posts: {
+        create: {
+          title: "Hello 345343",
+        },
+      },
+    },
+  });
+  console.log(user);
+
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(usersWithPosts, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
